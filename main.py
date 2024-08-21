a2ce72990dfd0dcc888f5c4a5cdc4c6951a5fcb0
ice-eca <ice_eca@mail.ru>
2024-08-21 18:29:25 +0500

    bug fix

--- main.py
+++ main.py
@@ -22,8 +22,10 @@ def enter_district(message):
     itembtn2 = types.InlineKeyboardButton(text='ВИЗ', callback_data='ВИЗ')
     itembtn3 = types.InlineKeyboardButton(text='Ботаника', callback_data='Ботаника')
     itembtn4 = types.InlineKeyboardButton(text='Академический', callback_data='Академический')
+    itembtn5 = types.InlineKeyboardButton(text='Верхняя Пышма', callback_data='Верхняя Пышма')
+    itembtn6 = types.InlineKeyboardButton(text='Клевер Парк', callback_data='Клевер Парк')
     
-    markup.add(itembtn1, itembtn2, itembtn3, itembtn4)
+    markup.add(itembtn1, itembtn2, itembtn3, itembtn4, itembtn5, itembtn6)
     bot.send_photo(message.chat.id, open('kiber1.png', 'rb'))
     bot.send_message(message.chat.id, 'Школа программирования для детей KIBERone Екатеринбург приветствует вас!\U0001F60A\n \nНа этой неделе мы проводим бесплатный мастер-класс по программированию для детей 6-14 лет\U0001F4BB\n \n\U00002705Ваш ребенок создаст свой первый мультфильм и запрограммирует своего героя в игре Майнкрафт\n \n\U00002705Расскажем, как избавить ребенка от игромании и научить компьютерной грамотности, чтобы подготовить к успешному будущему \n \n\U00002705Длительность занятия 60 минут. Все необходимое предоставим. Ничего брать с собой не нужно. \n \nВыберите удобный для Вас район\U0001F447' , reply_markup=markup)
     
